6703aaa9be1a4cfa9818d2490c557f33b4d92ebf
itsjasminn <muvvahhida@gmail.com>
2025-07-17 18:58:56 +0500

    updated

--- root/urls.py
+++ root/urls.py
@@ -1,19 +1,16 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-        path('admin/', admin.site.urls),
-        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-        # Optional UI:
-        path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
+urlpatterns += [
 
-
-urlpatterns+=[
-
-        path('api/v1/', include('authenticate.urls')),
-        path('api/v1/',include('apps.urls')),
+    path('api/v1/', include('authenticate.urls')),
+    path('api/v1/', include('apps.urls')),
 ]
